10190abc70d94cb44581ae79fc73fa5a6240a9ae
sebastien milou <milouseba@gmail.com>
2017-08-08 15:07:01 +0200

    exo 1 ajout font awesome, bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
 
-  # root to: 'pages#about'
+  root to: 'tasks#index'
 
   resources :tasks
-
-  # get 'tasks/index'
-
-  # get 'tasks/show'
-
-  # get 'tasks/new'
-
-  # get 'tasks/create'
-
-  # get 'tasks/edit'
-
-  # get 'tasks/update'
-
-  # get 'tasks/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
